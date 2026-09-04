859ae23a9cd185de8c44946d470f6e4b1d4c96e2
Josiah Hamm <jeffy9745@gmail.com>
2026-09-04 16:48:24 -0600

    quicksave

--- lab2/nao_lab2.py
+++ lab2/nao_lab2.py
@@ -24,7 +24,8 @@ class HumanGreeter(object):
         # Get the service ALMemory.
         self.memory = session.service("ALMemory")
         # Connect the event callback.
-        self.subscriber = self.memory.subscriber("FaceDetected")
+        print("Entered Init")
+        self.subscriber = self.memory.subscriber("PeoplePerception/JustArrived")
         self.subscriber.signal.connect(self.on_human_tracked)
         # Get the services ALTextToSpeech and ALFaceDetection.
         self.tts = session.service("ALTextToSpeech")
@@ -32,22 +33,20 @@ class HumanGreeter(object):
         self.face_detection.subscribe("HumanGreeter")
         self.got_face = False
 
-        self.arrivedSubscriber = self.memory.subscriber("s")
-        self.arrivedSubscriber.signal.connect(self.on_arrived)
-
     def on_human_tracked(self, value):
         """
         Callback for event FaceDetected.
         """
+        print("Josiah is an arch user")
         if value == []:  # empty value when the face disappears
             self.got_face = False
         elif not self.got_face:  # only speak the first time a face appears
             self.got_face = True
-            print ("I saw a face!")
+            print "I saw a face!"
             self.tts.say("Hello, you!")
             # First Field = TimeStamp.
             timeStamp = value[0]
-            print ("TimeStamp is: " + str(timeStamp))
+            print "TimeStamp is: " + str(timeStamp)
 
             # Second Field = array of face_Info's.
             faceInfoArray = value[1]
@@ -60,20 +59,20 @@ class HumanGreeter(object):
                 # Second Field = Extra info (empty for now).
                 faceExtraInfo = faceInfo[1]
 
-                print ("Face Infos :  alpha %.3f - beta %.3f" % (faceShapeInfo[1], faceShapeInfo[2]))
-                print ("Face Infos :  width %.3f - height %.3f" % (faceShapeInfo[3], faceShapeInfo[4]))
-                print ("Face Extra Infos :" + str(faceExtraInfo))
+                print "Face Infos :  alpha %.3f - beta %.3f" % (faceShapeInfo[1], faceShapeInfo[2])
+                print "Face Infos :  width %.3f - height %.3f" % (faceShapeInfo[3], faceShapeInfo[4])
+                print "Face Extra Infos :" + str(faceExtraInfo)
 
     def run(self):
         """
         Loop on, wait for events until manual interruption.
         """
-        print ("Starting HumanGreeter")
+        print "Starting HumanGreeter"
         try:
             while True:
                 time.sleep(1)
         except KeyboardInterrupt:
-            print ("Interrupted by user, stopping HumanGreeter")
+            print "Interrupted by user, stopping HumanGreeter"
             self.face_detection.unsubscribe("HumanGreeter")
             #stop
             sys.exit(0)
@@ -82,7 +81,7 @@ class HumanGreeter(object):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", type=str, default="10.1.65.187",
-                        help="Robot IP address. On robot or Local Naoqi: use '127.0.0.1'.")
+                        help="Robot IP address. On robot or Local Naoqi: use '10.1.65.187'.")
     parser.add_argument("--port", type=int, default=9559,
                         help="Naoqi port number")
 
