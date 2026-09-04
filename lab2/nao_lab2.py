74d54bc7c4b5c7711d74599cdd3b4c41cdb1e697
Josiah Hamm <jeffy9745@gmail.com>
2026-09-04 16:50:44 -0600

    update syntax

--- lab2/nao_lab2.py
+++ lab2/nao_lab2.py
@@ -42,11 +42,11 @@ class HumanGreeter(object):
             self.got_face = False
         elif not self.got_face:  # only speak the first time a face appears
             self.got_face = True
-            print "I saw a face!"
+            print("I saw a face!")
             self.tts.say("Hello, you!")
             # First Field = TimeStamp.
             timeStamp = value[0]
-            print "TimeStamp is: " + str(timeStamp)
+            print ("TimeStamp is: " + str(timeStamp))
 
             # Second Field = array of face_Info's.
             faceInfoArray = value[1]
@@ -59,20 +59,20 @@ class HumanGreeter(object):
                 # Second Field = Extra info (empty for now).
                 faceExtraInfo = faceInfo[1]
 
-                print "Face Infos :  alpha %.3f - beta %.3f" % (faceShapeInfo[1], faceShapeInfo[2])
-                print "Face Infos :  width %.3f - height %.3f" % (faceShapeInfo[3], faceShapeInfo[4])
-                print "Face Extra Infos :" + str(faceExtraInfo)
+                print ("Face Infos :  alpha %.3f - beta %.3f" % (faceShapeInfo[1], faceShapeInfo[2]))
+                print ("Face Infos :  width %.3f - height %.3f" % (faceShapeInfo[3], faceShapeInfo[4]))
+                print ("Face Extra Infos :" + str(faceExtraInfo))
 
     def run(self):
         """
         Loop on, wait for events until manual interruption.
         """
-        print "Starting HumanGreeter"
+        print ("Starting HumanGreeter")
         try:
             while True:
                 time.sleep(1)
         except KeyboardInterrupt:
-            print "Interrupted by user, stopping HumanGreeter"
+            print ("Interrupted by user, stopping HumanGreeter")
             self.face_detection.unsubscribe("HumanGreeter")
             #stop
             sys.exit(0)
